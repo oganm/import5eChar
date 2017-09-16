59289bbcd27abc6410eecbe0ec6f64653cb60b99
Ogan Mancarci <ogan.mancarci@gmail.com>
2017-09-16 03:50:35 -0700

    roxygen doing its thing

--- R/internal.R
+++ R/internal.R
@@ -11,7 +11,7 @@ logicConvert = function(x){
     x %>%  ogbox::replaceElement(c('true'=TRUE,'false'=FALSE)) %$% newVector %>% as.logical()
 }
 
-
+#' @export
 stat2mod = function(x){
     floor(x/2-5)
 }
