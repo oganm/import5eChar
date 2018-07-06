88a6c2bc5087361e4231dc0aaf66019ba65220e8
Ogan Mancarci <ogan.mancarci@gmail.com>
2018-07-05 23:31:06 -0700

    switch to staplr closes #9

--- R/prettyPDF.R
+++ R/prettyPDF.R
@@ -1,5 +1,5 @@
 # animation::pdftk(sourcePDF,operation = 'generate_fdf',output = fdfFile)
-
+# staplr::get_fields(input_filepath = sourcePDF)
 
 
 fdfEdit = function(x, field,fdf){
@@ -47,182 +47,153 @@ prettyPDF = function(file,char = getOption('defaultCharacter')){
 
     sourcePDF = system.file('character.pdf',package='import5eChar')
 
+    fields = readRDS(system.file('char.rds',package='import5eChar'))
 
     inChar = c('Str','Dex','Con','Int','Wis','Cha')
     inPDF =  c('str','dex','con','int','wis','cha')
 
     for(i in 1:length(char$abilityScores)){
         # ability scores
-        fdf = fdfEdit(
-            x = char$abilityScores[inChar[i]],
-            field = inPDF[i],
-            fdf = fdf)
+
+        fields[[inPDF[i]]]$value = char$abilityScores[inChar[i]]
+
 
         # ability mods
-        fdf = fdfEdit(
-            x = char$abilityMods[inChar[i]],
-            field = paste0(inPDF[i],'-mod'),fdf = fdf)
+
+        fields[[paste0(inPDF[i],'-mod')]]$value = char$abilityMods[inChar[i]]
+
     }
 
     # saves
     saves = saveBonus(char = char)
     for(i in 1:length(char$abilityScores)){
-        fdf = fdfEdit(
-            x = saves[i],
-            field = names(saves)[i] %>% tolower %>% paste0(.,'-save'),
-            fdf = fdf)
 
-        fdf = fdfEdit(
-            x = char$abilityProf[i],
-            field = names(char$abilityProf)[i] %>% tolower %>% paste0(.,'-save-check'),
-            fdf = fdf)
+
+        fields[[names(saves)[i] %>% tolower %>% paste0(.,'-save')]]$value = saves[i]
+
+
+
+        fields[[names(saves)[i] %>% tolower %>% paste0(.,'-save')]]$value = saves[i]
+
     }
 
 
     # AC
-    fdf = fdfEdit(
-        x = AC(char = char),
-        field = 'ac',
-        fdf = fdf)
+
+
+    fields[['ac']]$value =  AC(char = char)
+
 
     # init
-    fdf = fdfEdit(
-        x = initBonus(char = char),
-        field = 'initiative',
-        fdf = fdf)
+
+
+    fields[['initiative']]$value =  initBonus(char = char)
+
 
     # speed
-    fdf = fdfEdit(
-        x = char$baseSpeed + char$speedMiscMod,
-        field = 'speed',
-        fdf = fdf)
+
+
+    fields[['speed']]$value =  char$baseSpeed + char$speedMiscMod
+
 
     # HP
-    fdf = fdfEdit(
-        x = char$maxHealth,
-        field = 'hp-max',
-        fdf = fdf)
 
-    fdf = fdfEdit(
-        x = char$currentHealth,
-        field = 'hp-current',
-        fdf = fdf)
+
+    fields[['hp-max']]$value = char$maxHealth
+
+
+
+    fields[['hp-current']]$value =  char$currentHealth
+
 
     if(char$currentTempHP>0){
-        fdf = fdfEdit(
-            x = char$currentTempHP,
-            field = 'hp-temp',
-            fdf = fdf)
+
+        fields[['hp-temp']]$value =  char$currentTempHP
+
     }
 
-    char$currentHealth
-
     # hit dice
-    fdf = fdfEdit(
-        x = char$hitDice %>% paste(collapse=', '),
-        field = 'hd-total',
-        fdf = fdf)
+
+
+    fields[['hd-total']]$value = char$hitDice %>% paste(collapse=', ')
+
 
     # traits
-    fdf = fdfEdit(
-        x = char$personality$traits,
-        field = 'personality-traits',
-        fdf = fdf)
+
+
+    fields[['personality-traits']]$value = char$personality$traits
+
 
     # ideals
-    fdf = fdfEdit(
-        x = char$personality$ideals,
-        field = 'ideals',
-        fdf = fdf)
+
+    fields[['ideals']]$value = char$personality$ideals
+
     # bonds
-    fdf = fdfEdit(
-        x = char$personality$bonds,
-        field = 'bonds',
-        fdf = fdf)
+
+    fields[['bonds']]$value = char$personality$bonds
+
 
     # flaws
-    fdf = fdfEdit(
-        x = char$personality$flaws,
-        field = 'flaws',
-        fdf = fdf)
+
+
+    fields[['flaws']]$value = char$personality$flaws
+
 
     # features
-    fdf = fdfEdit(
-        x = char$Features %>% stringr::str_replace_all('•|\u{2022}','-') %>%
-            stringr::str_replace_all('(½)|(\u{00BD})','1/2') %>%
-            stringr::str_replace('(¾)|(\u{00BE})','3/4') %>%
-            stringr::str_replace('(¼)|(\u{00BC})','1/4'),
-        field = 'features-and-traits',
-        fdf = fdf)
 
-    fdf = fdfEdit(
-        x = char$notes %>% stringr::str_replace_all('•|\u{2022}','-') %>%
-            stringr::str_replace_all('(½)|(\u{00BD})','1/2') %>%
-            stringr::str_replace('(¾)|(\u{00BE})','3/4') %>%
-            stringr::str_replace('(¼)|(\u{00BC})','1/4'),
-        field = 'features-and-traits-2',
-        fdf = fdf)
+
+    fields[['features-and-traits']]$value =
+        char$Features %>% stringr::str_replace_all('•|\u{2022}','-') %>%
+        stringr::str_replace_all('(½)|(\u{00BD})','1/2') %>%
+        stringr::str_replace('(¾)|(\u{00BE})','3/4') %>%
+        stringr::str_replace('(¼)|(\u{00BC})','1/4')
+
+
+
+    fields[['features-and-traits-2']]$value =
+        char$notes %>% stringr::str_replace_all('•|\u{2022}','-') %>%
+        stringr::str_replace_all('(½)|(\u{00BD})','1/2') %>%
+        stringr::str_replace('(¾)|(\u{00BE})','3/4') %>%
+        stringr::str_replace('(¼)|(\u{00BC})','1/4')
 
     # class level
-    fdf = fdfEdit(
-        x = char$ClassField,
-        field = 'class-level',
-        fdf = fdf)
+
+    fields[['class-level']]$value = char$ClassField
+
 
     # background
-    fdf = fdfEdit(
-        x = char$Background,
-        field = 'background',
-        fdf = fdf)
+    fields[['background']]$value = char$Background
+
 
     # alignment
-    fdf = fdfEdit(
-        x = char$Alignment,
-        field = 'alignment',
-        fdf = fdf)
+    fields[['alignment']]$value = char$Alignment
+
 
     # race
-    fdf = fdfEdit(
-        x = char$Race,
-        field = 'race',
-        fdf = fdf)
+    fields[['race']]$value = char$Race
 
     # name
-    fdf = fdfEdit(
-        x = char$Name,
-        field = 'character-name',
-        fdf = fdf)
-    fdf = fdfEdit(
-        x = char$Name,
-        field = 'character-name-2',
-        fdf = fdf)
+    fields[['character-name']]$value = char$Name
+
+    fields[['character-name-2']]$value = char$Name
 
     # spells
     DC = spellDC(char = char)
     spellAttack  =  spellAttack(char = char)
-    fdf = fdfEdit(
-        x = DC,
-        field = 'spell-save-dc-1',
-        fdf = fdf)
 
-    fdf = fdfEdit(
-        x = spellAttack,
-        field = 'spell-attack-bonus-1',
-        fdf = fdf)
+    fields[['spell-save-dc-1']]$value = DC
 
-    fdf = fdfEdit(
-        x = names(spellAttack),
-        field = 'spellcasting-ability-1',
-        fdf = fdf)
+
+    fields[['spell-attack-bonus-1']]$value = spellAttack
+
+
+    fields[['spellcasting-ability-1']]$value =  names(spellAttack)
 
     slots = spellSlots(char)
 
     if(!is.null(char$spells)){
         for(i in 1:9){
-            fdf = fdfEdit(
-                x = slots[i],
-                field = glue::glue('spell-slots-{i}-1'),
-                fdf = fdf)
+            fields[[glue::glue('spell-slots-{i}-1')]]$value =  slots[i]
         }
 
         limits = c('0' = 8,
@@ -238,10 +209,7 @@ prettyPDF = function(file,char = getOption('defaultCharacter')){
         for(i in 0:9){
             spells  = char$spells %>% dplyr::filter(level ==i)
             for(t in seq_len(min(nrow(spells), limits[i %>% as.character()]))){
-                fdf = fdfEdit(
-                    x = spells$name[t],
-                    field = glue::glue('spells-{i}-{t}-1'),
-                    fdf = fdf)
+                fields[[glue::glue('spells-{i}-{t}-1')]]$value =  spells$name[t]
             }
         }
 
@@ -249,17 +217,13 @@ prettyPDF = function(file,char = getOption('defaultCharacter')){
     }
 
     # equipment
-    fdf = fdfEdit(
-        x = char$Equipment,
-        field = 'equipment',
-        fdf = fdf)
+    fields[['equipment']]$value = char$Equipment
+
 
     # gold
     for(x in names(char$currency)){
-        fdf = fdfEdit(
-            x = char$currency[[x]],
-            field =tolower(x),
-            fdf = fdf)
+        fields[[tolower(x)]]$value =char$currency[[x]]
+
     }
 
     # language and proficiency
@@ -287,19 +251,15 @@ prettyPDF = function(file,char = getOption('defaultCharacter')){
     weaponTable[,'Damage'] %<>% gsub('\\+\\-','-',x=.)
 
     for(i in 1:min(3,nrow(weaponTable))){
-        fdf = fdfEdit(
-            x = paste0(weaponTable[i,1],' (',weaponTable[i,5],')'),
-            field =paste0('weapon-name-',i),
-            fdf = fdf)
-        fdf = fdfEdit(
-            x = weaponTable[i,2],
-            field =paste0('weapon-attack-bonus-',i),
-            fdf = fdf)
 
-        fdf = fdfEdit(
-            x = paste0(weaponTable[i,3],'/',weaponTable[i,4]),
-            field =paste0('weapon-damage-',i),
-            fdf = fdf)
+        fields[[paste0('weapon-name-',i)]]$value = paste0(weaponTable[i,1],' (',weaponTable[i,5],')')
+
+
+        fields[[paste0('weapon-attack-bonus-',i)]]$value = weaponTable[i,2]
+
+
+        fields[[paste0('weapon-damage-',i)]]$value = paste0(weaponTable[i,3],'/',weaponTable[i,4])
+
     }
 
     moreWeapons = ''
@@ -312,10 +272,8 @@ prettyPDF = function(file,char = getOption('defaultCharacter')){
                              wepDat,
                              '\n')
     }
-    fdf = fdfEdit(
-        x = moreWeapons,
-        field ='attacks-and-spellcasting',
-        fdf = fdf)
+
+    fields[['attacks-and-spellcasting']]$value = moreWeapons
 
 
     # skills
@@ -323,41 +281,25 @@ prettyPDF = function(file,char = getOption('defaultCharacter')){
     pdfNames = skillBonuses %>% names %>% tolower %>% stringr::str_replace_all(' ','-')
     pdfCheckbox= paste0(pdfNames,'-check')
     for(i in 1:length(skillBonuses)){
-        fdf = fdfEdit(
-            x = skillBonuses[i],
-            field =pdfNames[i],
-            fdf = fdf)
-        fdf = fdfEdit(
-            x = char$skillProf[i],
-            field =pdfCheckbox[i],
-            fdf = fdf)
+
+        fields[[pdfNames[i]]]$value = skillBonuses[i]
+
+        fields[[pdfCheckbox[i]]]$value = char$skillProf[i] %>% {if(.){'Yes'}else{"Off"}}
+
     }
 
     # passive perception
-    fdf = fdfEdit(
-        x = 10 + skillBonuses['Perception'],
-        field = 'passive',
-        fdf = fdf)
+
+    fields[['passive']]$value = 10 + skillBonuses['Perception']
 
 
     # prof bonus
-    fdf = fdfEdit(
-        x = char$proficiencyBonus,
-        field = 'prof-bonus',
-        fdf = fdf)
+    fields[['prof-bonus']]$value = char$proficiencyBonus
 
 
-    fdf = fdfEdit(
-        x = profAndLang,
-        field ='other-profs',
-        fdf = fdf)
-
-    newFDF = tempfile()
-    write(fdf,newFDF)
-    animation::pdftk(sourcePDF,
-                     operation = paste0('fill_form ',newFDF),
-                     output = file)
+    fields[['other-profs']]$value = profAndLang
 
 
+    staplr::set_fields(sourcePDF,output_filepath = file,fields = fields)
 
 }
