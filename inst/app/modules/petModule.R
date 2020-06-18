909b8fe5c3d162bbedeb1b2bca8f2637d7b1bcc5
Ogan Mancarci <ogan.mancarci@gmail.com>
2020-06-17 21:52:55 -0700

    add AC to pets and prelude to skills and stuff

--- inst/app/modules/petModule.R
+++ inst/app/modules/petModule.R
@@ -49,6 +49,9 @@ monsters = monsteR::monsters
 
 monsters$`Steel Defender` = list(
     HP = 17, # minimum hp
+    AC = 15, # change at level 15
+    abilityScores = c(Str = 14, Dex = 12, Con = 14, Int = 4, Wis = 10, Cha = 6),
+    saves = c(Str = 2, Dex = 3, Con = 4, Int = -3, Wis = 0, Cha = -2),
     actions = list(
         `Force-Empowered Rend`= list(
             name = 'Force-Empowered Rend',
@@ -70,6 +73,23 @@ monsters$`Steel Defender` = list(
             saves  = c(Str = 2, Dex = 3, Con = 4, Int = -4, Wis = 0, Cha = -2))
         )
     )
+
+skillAttributes = c('Str',
+                    rep('Dex',3),
+                    rep('Int',5),
+                    rep('Wis',5),
+                    rep('Cha',4))
+
+
+monsters$`Steel Defender`$skills = stat2mod(monsters$`Steel Defender`$abilityScores)[skillAttributes]
+
+names(monsters$`Steel Defender`$skills) = names(monsters$Aboleth$skills)
+
+monsters$`Steel Defender`$skills['Athletics'] = 4
+monsters$`Steel Defender`$skills['Perception'] = 4
+
+
+
 monsters = c(monsters[length(monsters)],monsters[-length(monsters)])
 
 
@@ -152,39 +172,45 @@ petUI = function(id){
                        #              class = 'modButton',style = 'background-color:#6959CD'),
                        # actionButton(ns('setHealth'),'Set', class = 'modButton')),
                 column(6,
-                       shiny::selectInput(inputId = ns('monster'),label = 'Load SRD monster',choices = c('',names(monsters))))
+                       shiny::selectInput(inputId = ns('monster'),label = 'Load SRD monster',choices = c('',names(monsters))),
+                       numericInput(ns('AC'),label = 'AC',value = 10,width = '70px'))
 
             ),
             fluidRow(
-                div(textInput(ns('action1-name'),label = 'Attack',width = '150px'),style="display:inline-block"),
-                div(numericInput(ns('action1-attack'),label = 'Atk Bonus',value = 0,width = '70px'),style="display:inline-block"),
-                div(textInput(ns('action1-damage'), label = 'Damage',width = '90px'),style="display:inline-block"),
-                actionButton(ns('action1-button'),label = 'Attack!', class = 'weaponButton')
-            ),
-            fluidRow(
-                div(textInput(ns('action2-name'),label = '',width = '150px'),style="display:inline-block"),
-                div(numericInput(ns('action2-attack'),label = '',value = 0,width = '70px'),style="display:inline-block"),
-                div(textInput(ns('action2-damage'), label = '',width = '90px'),style="display:inline-block"),
-                actionButton(ns('action2-button'),label = 'Attack!', class = 'weaponButton')
-            ),
-            fluidRow(
-                div(textInput(ns('action3-name'),label = '',width = '150px'),style="display:inline-block"),
-                div(numericInput(ns('action3-attack'),label = '',value = 0,width = '70px'),style="display:inline-block"),
-                div(textInput(ns('action3-damage'), label = '',width = '90px'),style="display:inline-block"),
-                actionButton(ns('action3-button'),label = 'Attack!', class = 'weaponButton')
-            ),
-            fluidRow(
-                div(textInput(ns('action4-name'),label = '',width = '150px'),style="display:inline-block"),
-                div(numericInput(ns('action4-attack'),label = '',value = 0,width = '70px'),style="display:inline-block"),
-                div(textInput(ns('action4-damage'), label = '',width = '90px'),style="display:inline-block"),
-                actionButton(ns('action4-button'),label = 'Attack!', class = 'weaponButton')
-            ),
-            fluidRow(
-                div(textInput(ns('action5-name'),label = '',width = '150px'),style="display:inline-block"),
-                div(numericInput(ns('action5-attack'),label = '',value = 0,width = '70px'),style="display:inline-block"),
-                div(textInput(ns('action5-damage'), label = '',width = '90px'),style="display:inline-block"),
-                actionButton(ns('action5-button'),label = 'Attack!', class = 'weaponButton')
+                column(11,
+                       fluidRow(
+                           div(textInput(ns('action1-name'),label = 'Attack',width = '150px'),style="display:inline-block"),
+                           div(numericInput(ns('action1-attack'),label = 'Atk Bonus',value = 0,width = '70px'),style="display:inline-block"),
+                           div(textInput(ns('action1-damage'), label = 'Damage',width = '90px'),style="display:inline-block"),
+                           actionButton(ns('action1-button'),label = 'Attack!', class = 'weaponButton')
+                       ),
+                       fluidRow(
+                           div(textInput(ns('action2-name'),label = '',width = '150px'),style="display:inline-block"),
+                           div(numericInput(ns('action2-attack'),label = '',value = 0,width = '70px'),style="display:inline-block"),
+                           div(textInput(ns('action2-damage'), label = '',width = '90px'),style="display:inline-block"),
+                           actionButton(ns('action2-button'),label = 'Attack!', class = 'weaponButton')
+                       ),
+                       fluidRow(
+                           div(textInput(ns('action3-name'),label = '',width = '150px'),style="display:inline-block"),
+                           div(numericInput(ns('action3-attack'),label = '',value = 0,width = '70px'),style="display:inline-block"),
+                           div(textInput(ns('action3-damage'), label = '',width = '90px'),style="display:inline-block"),
+                           actionButton(ns('action3-button'),label = 'Attack!', class = 'weaponButton')
+                       ),
+                       fluidRow(
+                           div(textInput(ns('action4-name'),label = '',width = '150px'),style="display:inline-block"),
+                           div(numericInput(ns('action4-attack'),label = '',value = 0,width = '70px'),style="display:inline-block"),
+                           div(textInput(ns('action4-damage'), label = '',width = '90px'),style="display:inline-block"),
+                           actionButton(ns('action4-button'),label = 'Attack!', class = 'weaponButton')
+                       ),
+                       fluidRow(
+                           div(textInput(ns('action5-name'),label = '',width = '150px'),style="display:inline-block"),
+                           div(numericInput(ns('action5-attack'),label = '',value = 0,width = '70px'),style="display:inline-block"),
+                           div(textInput(ns('action5-damage'), label = '',width = '90px'),style="display:inline-block"),
+                           actionButton(ns('action5-button'),label = 'Attack!', class = 'weaponButton')
+                       )),
+                column(1)
             )
+
 
         )
     })
@@ -254,6 +280,10 @@ pet = function(input,output,session, char){
             monsters$`Steel Defender`$actions$`Force-Empowered Rend`$damage_bonus = char$proficiencyBonus
 
             monsters$`Steel Defender`$actions$Repair$damage_bonus = char$proficiencyBonus
+
+            if(level>=15){
+                monsters$`Steel Defender`$AC = 17
+            }
         }
 
         if(!is.null(input$monster) && input$monster!= ''){
@@ -268,6 +298,8 @@ pet = function(input,output,session, char){
 
 
             validActions = monsteR:::attackable(monster$actions)
+
+            updateNumericInput(session,'AC',value = monster$AC)
 
             for(i in seq(1,5)){
                 if(!is.na(validActions[i])){
